97c0d767aa17dde625496875d11c66995b59c80a
Mohammed Rabil <rabil@techie.com>
2022-12-31 13:58:26 +0530

    Setup basic config

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+// eslint-disable-next-line no-undef
+app.use(express.static(path.join(__dirname, "public")));
+// eslint-disable-next-line no-undef
+app.set("views", path.join(__dirname, "views"));
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.render("pages/index");
     }
 );
 
