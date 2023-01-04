ea54ddaa8aab83b320d342de8e17ba991c408272
Mohammed Rabil <rabil@techie.com>
2023-01-04 19:28:32 +0530

    Add new models
    - Votes
    - Voters

---
+++ models/votes.js
@@ -0,0 +1,53 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Votes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Elections, { foreignKey: 'electionId' });
+      this.belongsTo(models.Questions, { foreignKey: 'questionId' });
+      this.belongsTo(models.Answers, { foreignKey: 'answerId' });
+    }
+  }
+  Votes.init({
+    voterId: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    electionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Elections',
+        key: 'id',
+      },
+    },
+    questionId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Questions',
+        key: 'id',
+      },
+    },
+    answerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Answers',
+        key: 'id',
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Votes',
+  });
+  return Votes;
+};
