0227dee781b7ee5f9922cc42b8d4b3cf24561096
Mohammed Rabil <rabil@techie.com>
2023-01-22 17:20:56 +0530

    Refactor vote cast success message

--- app.js
+++ app.js
@@ -148,10 +148,10 @@ app.use((request, response, next) => {
   // Restrict access to /vote to logged in voters
   if (request.user && request.user.voterId) {
     if (
-      !request.path === "/" &&
       !request.path.endsWith("/vote") &&
       !request.path.endsWith("/logout") &&
-      !request.path.endsWith("/results")
+      !request.path.endsWith("/results") &&
+      !request.path.endsWith("/message")
     ) {
       return response.status(403).send("Forbidden");
     }
@@ -685,16 +685,16 @@ app.post(
           });
         });
         req.accepts("html")
-          ? req.flash("success", "Vote cast successfully") && res.redirect("/")
+          ? req.flash("success", "Vote cast successfully. Please wait for the results") && res.redirect("/message")
           : res.json({ message: "Vote cast successfully" });
       } catch (error) {
         req.accepts("html")
-          ? req.flash("error", error.message) && res.redirect("/")
+          ? req.flash("error", error.message) && res.redirect("/message")
           : res.status(422).json({ error: error.message });
       }
     } else {
       req.accepts("html")
-        ? req.flash("error", "Election not found")
+        ? req.flash("error", "Election not found") && res.redirect("/message")
         : res.status(404).json({ error: "Election not found" });
     }
   }
@@ -776,5 +776,9 @@ app.put(
     }
   }
 );
+
+app.get("/message" , (req, res) => {
+  res.render("pages/message");
+});
 
 module.exports = app;
